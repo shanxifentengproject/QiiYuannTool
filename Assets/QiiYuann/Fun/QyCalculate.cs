62c09cbe2998444123781957fd425a13fa6a9ac2
syq <shenyongqiang2008@126.com>
2020-05-29 17:41:22 +0800

    添加线性比例计算组件

---
+++ Assets/QiiYuann/Fun/QyCalculate.cs
@@ -0,0 +1,26 @@
+﻿
+/// <summary>
+/// 数学计算类
+/// </summary>
+public class QyCalculate
+{
+    /// <summary>
+    /// 线性计算数据
+    /// </summary>
+    [System.Serializable]
+    public class LineData
+    {
+        public float m_MaxValue = 20f, m_MinValue = 3f;
+        public float m_MaxDis = 30f, m_MinDis = 3f;
+        internal float m_Key;
+        internal void CalculateKey()
+        {
+            m_Key = (m_MaxValue - m_MinValue) / (m_MaxDis - m_MinDis);
+        }
+
+        internal float GetCurValue(float curDis)
+        {
+            return m_Key * (curDis - m_MinDis) + m_MinValue;
+        }
+    }
+}
